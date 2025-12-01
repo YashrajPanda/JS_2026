341dd4b50ddf0bb3b56999a4332101f611678ebf
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-01 19:51:42 +0000

    dataTypes in js

---
+++ 1_Basics/2_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all js code as newer version
+
+// alert  ( 3+3 ) we use alert to show pop up message, we are using modejs, not browser
+
+console.log ( 3
+    +
+    3
+) // code readability should be high
+
+console.log("Yash")
+
+let name = "Yashraj"; // string datatype
+let age = 21; // number datatype
+let isLoggedIn = false; // boolean datatype
+let state = null; // null datatype
+
+// number ==> 2 to the power 53
+// bigint ==> larger than 2 to the power 53
+// string ==> '', "", ``
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> variable not assigned any value
+// symbol ==> unique identifier
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); //object
+console.log(typeof undefinedVariable); //undefined
+
