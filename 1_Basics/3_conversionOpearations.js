27cdbccfe58ea8be43f0118a9aa3860457d0c391
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-01 20:15:46 +0000

    conversions of datatypes

---
+++ 1_Basics/3_conversionOpearations.js
@@ -0,0 +1,32 @@
+let score = "yash";
+// let score = "100";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 , false -> 0
+
+let isLoggedIn = "yash";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// "" -> false
+// "yash" -> true
+// 0 -> false , any other number -> true
+// null -> false
+// undefined -> false
+
+let someNumber = 123;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 123 -> "123"
