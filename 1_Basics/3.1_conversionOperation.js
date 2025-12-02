b94f43dcabac96e5fe1373aaf97b7eacc288681b
YashrajPanda <yashrajpanda320@gmail.com>
2025-12-02 06:19:45 +0000

    conversions of js with prefix and postfix concepts

---
+++ 1_Basics/3.1_conversionOperation.js
@@ -0,0 +1,37 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Yash"
+let str2 = " hello"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 3)
+// console.log(1 + "4")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100;
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter)
+
+// link to study : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
